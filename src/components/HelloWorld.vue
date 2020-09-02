e0e4f26ec3fb78db842290c6e67ece26fc242eb7
ryk01 <ryk2100@163.com>
2020-09-02 18:22:28 +0800

    add sass vue-router vuex axios

---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,24 @@
+<template>
+  <div class="hello">
+    <h1>{{ msg }}</h1>
+    <p>
+      <a
+        href="https://cli.vuejs.org"
+        target="_blank"
+        rel="noopener"
+      >vue-cli documentation</a>
+    </p>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "HelloWorld",
+  props: {
+    msg: String,
+  },
+};
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped></style>
---
+++ vue.config.js
@@ -0,0 +1,11 @@
+// vue.config.js
+module.exports = {
+  publicPath:
+    process.env.NODE_ENV === "production" ? "/production-sub-path/" : "/",
+  devServer: {
+    proxy: "http://localhost:4000",
+  },
+  css: {
+    requireModuleExtension: false,
+  },
+};
